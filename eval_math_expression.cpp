492783ce9aa90fadcca94b0ea2f89f808225eb7a
hapepo23 <github@hapepo.de>
2026-03-08 17:15:15 +0100

    update 2026-03-08 17:15 CET

--- eval_math_expression.cpp
+++ eval_math_expression.cpp
@@ -7,79 +7,74 @@ https://www.codewars.com/kata/52a78825cdfc2cfc87000005
 #include <cctype>
 #include <string>
 
-class Parser {
- public:
-  Parser(const std::string& s) : str(s), pos(0) {}
+static std::string str;
+static size_t pos;
 
-  double parse() { return parseExpression(); }
+static double parseFactor();
+static double parseTerm();
+static double parseExpression();
+static void skipSpaces();
 
- private:
-  const std::string& str;
-  size_t pos;
-
-  void skipSpaces() {
-    while (pos < str.size() && std::isspace(str[pos]))
-      pos++;
+static double parseFactor() {
+  skipSpaces();
+  if (pos < str.size() && str[pos] == '-') {
+    pos++;
+    return -parseFactor();
   }
-
-  double parseExpression() {
-    double value = parseTerm();
-    while (true) {
-      skipSpaces();
-      if (pos < str.size() && (str[pos] == '+' || str[pos] == '-')) {
-        char op = str[pos++];
-        double rhs = parseTerm();
-        if (op == '+')
-          value += rhs;
-        else
-          value -= rhs;
-      } else
-        break;
-    }
-    return value;
-  }
-
-  double parseTerm() {
-    double value = parseFactor();
-    while (true) {
-      skipSpaces();
-      if (pos < str.size() && (str[pos] == '*' || str[pos] == '/')) {
-        char op = str[pos++];
-        double rhs = parseFactor();
-        if (op == '*')
-          value *= rhs;
-        else
-          value /= rhs;
-      } else
-        break;
-    }
-    return value;
-  }
-
-  double parseFactor() {
+  if (pos < str.size() && str[pos] == '(') {
+    pos++;
+    double value = parseExpression();
     skipSpaces();
-    // unary minus
-    if (pos < str.size() && str[pos] == '-') {
-      pos++;
-      return -parseFactor();
-    }
-    // parentheses
-    if (pos < str.size() && str[pos] == '(') {
-      pos++;
-      double value = parseExpression();
-      skipSpaces();
-      pos++;  // skip ')'
-      return value;
-    }
-    // number
-    size_t start = pos;
-    while (pos < str.size() && (std::isdigit(str[pos]) || str[pos] == '.'))
-      pos++;
-    return std::stod(str.substr(start, pos - start));
+    pos++;
+    return value;
   }
-};
+  size_t start = pos;
+  while (pos < str.size() && (std::isdigit(str[pos]) || str[pos] == '.'))
+    pos++;
+  return std::stod(str.substr(start, pos - start));
+}
+
+static double parseTerm() {
+  double value = parseFactor();
+  while (true) {
+    skipSpaces();
+    if (pos < str.size() && (str[pos] == '*' || str[pos] == '/')) {
+      char op = str[pos++];
+      double rhs = parseFactor();
+      if (op == '*')
+        value *= rhs;
+      else
+        value /= rhs;
+    } else
+      break;
+  }
+  return value;
+}
+
+static double parseExpression() {
+  double value = parseTerm();
+  while (true) {
+    skipSpaces();
+    if (pos < str.size() && (str[pos] == '+' || str[pos] == '-')) {
+      char op = str[pos++];
+      double rhs = parseTerm();
+      if (op == '+')
+        value += rhs;
+      else
+        value -= rhs;
+    } else
+      break;
+  }
+  return value;
+}
+
+static void skipSpaces() {
+  while (pos < str.size() && std::isspace(str[pos]))
+    pos++;
+}
 
 double calc(const std::string& expression) {
-  Parser parser(expression);
-  return parser.parse();
+  pos = 0;
+  str = expression;
+  return parseExpression();
 }
